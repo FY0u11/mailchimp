5ca1bfc69dec78618bddf2a602f0fc7bc9577227
Sergey Strigin <darkauron1997@gmail.com>
2022-07-09 12:57:20 +0600

    add description for parameters in api in phpdocs

--- src/Api/AccountExports.php
+++ src/Api/AccountExports.php
@@ -12,10 +12,13 @@ class AccountExports extends AbstractApi
     /**
      * Get a list of account exports for a given account.
      *
-     * @param array|null $fields
-     * @param array|null $exclude_fields
-     * @param int|null $count
-     * @param int|null $offset
+     * @param array|null $fields          A comma-separated list of fields to return. Reference parameters of
+     *                                    sub-objects with dot notation.
+     * @param array|null $exclude_fields  A comma-separated list of fields to exclude. Reference parameters of
+     *                                    sub-objects with dot notation.
+     * @param int|null $count             The number of records to return. Default value is 10. Maximum value is 1000
+     * @param int|null $offset            Used for pagination, this is the number of records from a collection to skip.
+     *                                    Default value is 0.
      * @return array
      */
     public function getAll(
@@ -33,9 +36,11 @@ class AccountExports extends AbstractApi
     /**
      * Get information about a specific account export.
      *
-     * @param string $exportId
-     * @param array|null $fields
-     * @param array|null $exclude_fields
+     * @param string $exportId            The unique id for the account export.
+     * @param array|null $fields          A comma-separated list of fields to return. Reference parameters of
+     *                                    sub-objects with dot notation.
+     * @param array|null $exclude_fields  A comma-separated list of fields to exclude. Reference parameters of
+     *                                    sub-objects with dot notation.
      * @return array
      */
     public function getById(string $exportId, ?array $fields=null, ?array $exclude_fields=null): array
@@ -49,8 +54,10 @@ class AccountExports extends AbstractApi
     /**
      * Create a new account export in your Mailchimp account.
      *
-     * @param array $include_stages
-     * @param string|null $since_timestamp
+     * @param array $include_stages         The stages of an account export to include.
+     * @param string|null $since_timestamp  An ISO 8601 date that will limit the export to only records created after a
+     *                                      given time. For instance, the reports stage will contain any campaign sent
+     *                                      after the given timestamp. Audiences, however, are excluded from this limit.
      * @return array
      */
     public function add(array $include_stages, ?string $since_timestamp = null): array
